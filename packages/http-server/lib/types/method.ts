9e94b4da12849a9e43a652b7d0ba98fd93c23eb8
partyka <partyka95@icloud.com>
2022-03-07 00:58:54 +0100

    improve http-server

--- packages/http-server/lib/types/method.ts
+++ packages/http-server/lib/types/method.ts
@@ -1,1 +1,1 @@
-export type Method = 'CONNECT' | 'DELETE' | 'GET' | 'HEAD' | 'OPTIONS' | 'PATCH' | 'POST' | 'PUT' | 'TRACE';
+export type Method = 'DELETE' | 'GET' | 'HEAD' | 'OPTIONS' | 'PATCH' | 'POST' | 'PUT';
