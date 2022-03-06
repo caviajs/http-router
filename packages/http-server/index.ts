bdd4fef290a0d543f1d1ef6fba02a9b2ae832900
partyka <partyka95@icloud.com>
2022-03-06 16:27:16 +0100

    public api

--- packages/http-server/index.ts
+++ packages/http-server/index.ts
@@ -1,6 +1,20 @@
 import 'reflect-metadata';
 
+export * from './lib/decorators/body';
 export * from './lib/decorators/controller';
+export * from './lib/decorators/cookies';
+export * from './lib/decorators/delete';
+export * from './lib/decorators/get';
+export * from './lib/decorators/head';
+export * from './lib/decorators/headers';
+export * from './lib/decorators/options';
+export * from './lib/decorators/params';
+export * from './lib/decorators/patch';
+export * from './lib/decorators/post';
+export * from './lib/decorators/put';
+export * from './lib/decorators/query';
+export * from './lib/decorators/req';
+export * from './lib/decorators/res';
 
 export * from './lib/interceptors/body-parser-interceptor';
 
