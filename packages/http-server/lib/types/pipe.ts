30cf3467d050403748cb479d1a03d2f850f67fd9
partyka <partyka95@icloud.com>
2022-03-11 23:32:30 +0100

    pipe type

--- packages/http-server/lib/types/pipe.ts
+++ packages/http-server/lib/types/pipe.ts
@@ -1,10 +1,8 @@
-import { Type } from '@caviajs/core';
-
 export interface Pipe {
   transform(value: any, metadata: PipeMetadata): any;
 }
 
 export interface PipeMetadata {
   args: any[];
-  metaType: Type | undefined;
+  metaType: any;
 }
