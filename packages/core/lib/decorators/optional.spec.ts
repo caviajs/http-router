0b558b1a460f1f066cbc4910cb99b8b011db7310
partyka <partyka95@icloud.com>
2022-03-12 00:04:56 +0100

    improve optional tests

--- packages/core/lib/decorators/optional.spec.ts
+++ packages/core/lib/decorators/optional.spec.ts
@@ -1,6 +1,16 @@
 import { OPTIONAL_METADATA, Optional, OptionalMetadata } from './optional';
 
 describe('@Optional', () => {
+  let defineMetadataSpy: jest.SpyInstance;
+
+  beforeEach(() => {
+    defineMetadataSpy = jest.spyOn(Reflect, 'defineMetadata');
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should add the appropriate metadata while using decorator', () => {
     class Foo {
       constructor(
@@ -12,10 +22,9 @@ describe('@Optional', () => {
       }
     }
 
-    const metadata: OptionalMetadata = Reflect.getMetadata(OPTIONAL_METADATA, Foo);
+    const optionalMetadata: OptionalMetadata = new Map().set(1, true).set(3, true);
 
-    expect(metadata.size).toBe(2);
-    expect(metadata.get(1)).toBe(true);
-    expect(metadata.get(3)).toBe(true);
+    expect(defineMetadataSpy).toHaveBeenCalledTimes(2);
+    expect(defineMetadataSpy).toHaveBeenLastCalledWith(OPTIONAL_METADATA, optionalMetadata, Foo);
   });
 });
