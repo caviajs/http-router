8fb14ef419a95484aa65cbd16a492d450da46718
partyka <partyka95@icloud.com>
2022-03-23 00:49:00 +0100

    improve testing

--- packages/testing/lib/http-server-test.ts
+++ packages/testing/lib/http-server-test.ts
@@ -1,82 +1,116 @@
-import { httpRequest, HttpRequestOptions, HttpRequestResult } from '@caviajs/common';
+import { Http } from '@caviajs/common';
 import http from 'http';
 import https from 'https';
 import { AddressInfo } from 'net';
 import { Readable } from 'stream';
 
+function composeUrl(server: http.Server | https.Server, url: string): string {
+  const port = (server.address() as AddressInfo).port;
+  const protocol = server instanceof https.Server ? 'https' : 'http';
+
+  return `${ protocol }://127.0.0.1:${ port }/${ url.replace(/^\//, '') }`;
+}
+
 export class HttpServerTest {
-  public static delete(server: http.Server | https.Server, url: string, options?: Omit<{ responseType?: 'buffer' } & TestRequestOptions, 'method' | 'url'>): Promise<TestRequestResult<Buffer>>;
-  public static delete<T = any>(server: http.Server | https.Server, url: string, options?: Omit<{ responseType?: 'json' } & TestRequestOptions, 'method' | 'url'>): Promise<TestRequestResult<T>>;
-  public static delete(server: http.Server | https.Server, url: string, options?: Omit<{ responseType?: 'stream' } & TestRequestOptions, 'method' | 'url'>): Promise<TestRequestResult<Readable>>;
-  public static delete(server: http.Server | https.Server, url: string, options?: Omit<{ responseType?: 'text' } & TestRequestOptions, 'method' | 'url'>): Promise<TestRequestResult<string>>;
-  public static delete(server: http.Server | https.Server, url: string, options?: Omit<TestRequestOptions, 'method' | 'url'>): Promise<TestRequestResult<any>> {
-    return this.request(server, { ...options, method: 'DELETE', url: url } as any);
-  }
-
-  public static get(server: http.Server | https.Server, url: string, options?: Omit<{ responseType?: 'buffer' } & TestRequestOptions, 'method' | 'url'>): Promise<TestRequestResult<Buffer>>;
-  public static get<T = any>(server: http.Server | https.Server, url: string, options?: Omit<{ responseType?: 'json' } & TestRequestOptions, 'method' | 'url'>): Promise<TestRequestResult<T>>;
-  public static get(server: http.Server | https.Server, url: string, options?: Omit<{ responseType?: 'stream' } & TestRequestOptions, 'method' | 'url'>): Promise<TestRequestResult<Readable>>;
-  public static get(server: http.Server | https.Server, url: string, options?: Omit<{ responseType?: 'text' } & TestRequestOptions, 'method' | 'url'>): Promise<TestRequestResult<string>>;
-  public static get(server: http.Server | https.Server, url: string, options?: Omit<TestRequestOptions, 'method' | 'url'>): Promise<TestRequestResult<any>> {
-    return this.request(server, { ...options, method: 'GET', url: url } as any);
-  }
-
-  public static patch(server: http.Server | https.Server, url: string, body?: any, options?: Omit<{ responseType?: 'buffer' } & TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<Buffer>>;
-  public static patch<T = any>(server: http.Server | https.Server, url: string, body?: any, options?: Omit<{ responseType?: 'json' } & TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<T>>;
-  public static patch(server: http.Server | https.Server, url: string, body?: any, options?: Omit<{ responseType?: 'stream' } & TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<Readable>>;
-  public static patch(server: http.Server | https.Server, url: string, body?: any, options?: Omit<{ responseType?: 'text' } & TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<string>>;
-  public static patch(server: http.Server | https.Server, url: string, body?: any, options?: Omit<TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<any>> {
-    return this.request(server, { ...options, body: body, method: 'PATCH', url: url } as any);
-  }
-
-  public static post(server: http.Server | https.Server, url: string, body?: any, options?: Omit<{ responseType?: 'buffer' } & TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<Buffer>>;
-  public static post<T = any>(server: http.Server | https.Server, url: string, body?: any, options?: Omit<{ responseType?: 'json' } & TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<T>>;
-  public static post(server: http.Server | https.Server, url: string, body?: any, options?: Omit<{ responseType?: 'stream' } & TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<Readable>>;
-  public static post(server: http.Server | https.Server, url: string, body?: any, options?: Omit<{ responseType?: 'text' } & TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<string>>;
-  public static post(server: http.Server | https.Server, url: string, body?: any, options?: Omit<TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<any>> {
-    return this.request(server, { ...options, body: body, method: 'POST', url: url } as any);
-  }
-
-  public static put(server: http.Server | https.Server, url: string, body?: any, options?: Omit<{ responseType?: 'buffer' } & TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<Buffer>>;
-  public static put<T = any>(server: http.Server | https.Server, url: string, body?: any, options?: Omit<{ responseType?: 'json' } & TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<T>>;
-  public static put(server: http.Server | https.Server, url: string, body?: any, options?: Omit<{ responseType?: 'stream' } & TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<Readable>>;
-  public static put(server: http.Server | https.Server, url: string, body?: any, options?: Omit<{ responseType?: 'text' } & TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<string>>;
-  public static put(server: http.Server | https.Server, url: string, body?: any, options?: Omit<TestRequestOptions, 'body' | 'method' | 'url'>): Promise<TestRequestResult<any>> {
-    return this.request(server, { ...options, body: body, method: 'PUT', url: url } as any);
-  }
-
-  public static async request(server: http.Server | https.Server, options: { responseType?: 'buffer' } & TestRequestOptions): Promise<TestRequestResult<Buffer>>;
-  public static async request<T>(server: http.Server | https.Server, options: { responseType?: 'json' } & TestRequestOptions): Promise<TestRequestResult<T>>;
-  public static async request(server: http.Server | https.Server, options: { responseType?: 'stream' } & TestRequestOptions): Promise<TestRequestResult<Readable>>;
-  public static async request(server: http.Server | https.Server, options: { responseType?: 'text' } & TestRequestOptions): Promise<TestRequestResult<string>>;
-  public static async request(server: http.Server | https.Server, options: TestRequestOptions): Promise<TestRequestResult<any>> {
+  public static delete(server: http.Server | https.Server, url: string, options?: TestRequestOptions & { responseType?: 'buffer' }): Promise<TestRequestResponse<Buffer>>;
+  public static delete<T = any>(server: http.Server | https.Server, url: string, options?: TestRequestOptions & { responseType?: 'json' }): Promise<TestRequestResponse<T>>;
+  public static delete(server: http.Server | https.Server, url: string, options?: TestRequestOptions & { responseType?: 'stream' }): Promise<TestRequestResponse<Readable>>;
+  public static delete(server: http.Server | https.Server, url: string, options?: TestRequestOptions & { responseType?: 'text' }): Promise<TestRequestResponse<string>>;
+  public static delete(server: http.Server | https.Server, url: string, options?: TestRequestOptions): Promise<TestRequestResponse<any>> {
     if (server.address() === null) {
       server.listen();
     }
 
-    const port = (server.address() as AddressInfo).port;
-    const protocol = server instanceof https.Server ? 'https' : 'http';
-
-    const httpRequestOptions: HttpRequestOptions = {
-      body: options.body,
-      headers: options.headers,
-      params: options.params,
-      method: options.method,
-      responseType: options.responseType as any,
-      timeout: options.timeout,
-      url: `${ protocol }://127.0.0.1:${ port }/${ options.url.replace(/^\//, '') }`,
-    };
-
-    return httpRequest(httpRequestOptions as any)
+    return Http
+      .request({ ...options, method: 'DELETE', url: composeUrl(server, url) })
       .then(result => {
         server.close();
 
-        return {
-          body: result.body,
-          headers: result.headers,
-          statusCode: result.statusCode,
-          statusMessage: result.statusMessage,
-        };
+        return result;
+      })
+      .catch(error => {
+        throw error;
+      });
+  }
+
+  public static get(server: http.Server | https.Server, url: string, options?: TestRequestOptions & { responseType?: 'buffer' }): Promise<TestRequestResponse<Buffer>>;
+  public static get<T = any>(server: http.Server | https.Server, url: string, options?: TestRequestOptions & { responseType?: 'json' }): Promise<TestRequestResponse<T>>;
+  public static get(server: http.Server | https.Server, url: string, options?: TestRequestOptions & { responseType?: 'stream' }): Promise<TestRequestResponse<Readable>>;
+  public static get(server: http.Server | https.Server, url: string, options?: TestRequestOptions & { responseType?: 'text' }): Promise<TestRequestResponse<string>>;
+  public static get(server: http.Server | https.Server, url: string, options?: TestRequestOptions): Promise<TestRequestResponse<any>> {
+    if (server.address() === null) {
+      server.listen();
+    }
+
+    return Http
+      .request({ ...options, method: 'GET', url: composeUrl(server, url) })
+      .then(result => {
+        server.close();
+
+        return result;
+      })
+      .catch(error => {
+        throw error;
+      });
+  }
+
+  public static patch(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions & { responseType?: 'buffer' }): Promise<TestRequestResponse<Buffer>>;
+  public static patch<T = any>(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions & { responseType?: 'json' }): Promise<TestRequestResponse<T>>;
+  public static patch(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions & { responseType?: 'stream' }): Promise<TestRequestResponse<Readable>>;
+  public static patch(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions & { responseType?: 'text' }): Promise<TestRequestResponse<string>>;
+  public static patch(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions): Promise<TestRequestResponse<any>> {
+    if (server.address() === null) {
+      server.listen();
+    }
+
+    return Http
+      .request({ ...options, body: body, method: 'PATCH', url: composeUrl(server, url) })
+      .then(result => {
+        server.close();
+
+        return result;
+      })
+      .catch(error => {
+        throw error;
+      });
+  }
+
+  public static post(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions & { responseType?: 'buffer' }): Promise<TestRequestResponse<Buffer>>;
+  public static post<T = any>(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions & { responseType?: 'json' }): Promise<TestRequestResponse<T>>;
+  public static post(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions & { responseType?: 'stream' }): Promise<TestRequestResponse<Readable>>;
+  public static post(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions & { responseType?: 'text' }): Promise<TestRequestResponse<string>>;
+  public static post(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions): Promise<TestRequestResponse<any>> {
+    if (server.address() === null) {
+      server.listen();
+    }
+
+    return Http
+      .request({ ...options, body: body, method: 'POST', url: composeUrl(server, url) })
+      .then(result => {
+        server.close();
+
+        return result;
+      })
+      .catch(error => {
+        throw error;
+      });
+  }
+
+  public static put(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions & { responseType?: 'buffer' }): Promise<TestRequestResponse<Buffer>>;
+  public static put<T = any>(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions & { responseType?: 'json' }): Promise<TestRequestResponse<T>>;
+  public static put(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions & { responseType?: 'stream' }): Promise<TestRequestResponse<Readable>>;
+  public static put(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions & { responseType?: 'text' }): Promise<TestRequestResponse<string>>;
+  public static put(server: http.Server | https.Server, url: string, body?: any, options?: TestRequestOptions): Promise<TestRequestResponse<any>> {
+    if (server.address() === null) {
+      server.listen();
+    }
+
+    return Http
+      .request({ ...options, body: body, method: 'PUT', url: composeUrl(server, url) })
+      .then(result => {
+        server.close();
+
+        return result;
       })
       .catch(error => {
         throw error;
@@ -84,6 +118,15 @@ export class HttpServerTest {
   }
 }
 
-export type TestRequestOptions = HttpRequestOptions;
+export interface TestRequestOptions {
+  headers?: { [key: string]: string | number };
+  params?: { [key: string]: string };
+  timeout?: number;
+}
 
-export type TestRequestResult<T> = HttpRequestResult<T>;
+export interface TestRequestResponse<T> {
+  readonly body: T;
+  readonly headers: { readonly [key: string]: string | string[] };
+  readonly statusCode: number;
+  readonly statusMessage: string;
+}
