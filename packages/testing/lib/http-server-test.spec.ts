9313709fa6885bfa34fbc6ce48995a9c289c76ca
partyka <partyka95@icloud.com>
2022-03-24 23:54:08 +0100

    http server test

--- packages/testing/lib/http-server-test.spec.ts
+++ packages/testing/lib/http-server-test.spec.ts
@@ -1,5 +1,136 @@
+import { Http } from '@caviajs/common';
+import { HttpServerTest, HttpOptions, HttpResponse } from './http-server-test';
+import http from 'http';
+import net from 'net';
+
+const method = 'POST';
+const url: string = 'api/users';
+const httpOptions: HttpOptions = {
+  body: { foo: 'bar' },
+  headers: { 'X-Foo': 'Foo' },
+  params: { foo: 'bar' },
+  timeout: 12500,
+};
+const httpResponse: HttpResponse<undefined> = {
+  body: undefined,
+  headers: {},
+  statusCode: 200,
+  statusMessage: 'OK',
+};
+
+const httpServerPort: number = 4200;
+const httpServerProtocol: string = 'http';
+const httpServerUrl: string = `${ httpServerProtocol }://127.0.0.1:${ httpServerPort }`;
+
 describe('HttpServerTest', () => {
-  it('should', () => {
-    expect(1).toBe(1);
+  let httpRequestSpy: jest.SpyInstance;
+  let httpServer: http.Server;
+
+  beforeEach(() => {
+    httpRequestSpy = jest.spyOn(Http, 'request').mockReturnValue(Promise.resolve(httpResponse));
+    httpServer = http.createServer();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('request', () => {
+    it('should execute http.Server.close after request', async () => {
+      const httpServerCloseSpy: jest.SpyInstance = jest.spyOn(http.Server.prototype, 'close').mockImplementation(jest.fn());
+
+      await HttpServerTest.request(httpServer, method, url, httpOptions);
+
+      expect(httpServerCloseSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should execute http.Server.listen if the http.Server.address return null', async () => {
+      jest.spyOn(http.Server.prototype, 'address').mockReturnValue(null);
+
+      const httpServerListenSpy: jest.SpyInstance = jest.spyOn(http.Server.prototype, 'listen').mockImplementation(jest.fn());
+
+      await HttpServerTest.request(httpServer, method, url, httpOptions);
+
+      expect(httpServerListenSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should not execute http.Server.listen if the http.Server.address return address info', async () => {
+      jest.spyOn(http.Server.prototype, 'address').mockReturnValue({ port: httpServerPort } as net.AddressInfo);
+
+      const httpServerListenSpy: jest.SpyInstance = jest.spyOn(http.Server.prototype, 'listen').mockImplementation(jest.fn());
+
+      await HttpServerTest.request(httpServer, method, url, httpOptions);
+
+      expect(httpServerListenSpy).toHaveBeenCalledTimes(0);
+    });
+
+    describe('without response type', () => {
+      it('should execute Http.request with the correct options and return appropriate data', async () => {
+        const response = await HttpServerTest.request(httpServer, method, url, httpOptions);
+
+        expect(response).toEqual(httpResponse);
+        expect(httpRequestSpy).toHaveBeenNthCalledWith(1, {
+          ...httpOptions,
+          method: method,
+          url: `${ httpServerUrl }/${ url }`,
+        });
+      });
+    });
+
+    describe('buffer response type', () => {
+      it('should execute Http.request with the correct options and return appropriate data', async () => {
+        const response = await HttpServerTest.request(httpServer, method, url, { ...httpOptions, responseType: 'buffer' });
+
+        expect(response).toEqual(httpResponse);
+        expect(httpRequestSpy).toHaveBeenNthCalledWith(1, {
+          ...httpOptions,
+          responseType: 'buffer',
+          method: method,
+          url: `${ httpServerUrl }/${ url }`,
+        });
+      });
+    });
+
+    describe('json response type', () => {
+      it('should execute Http.request with the correct options and return appropriate data', async () => {
+        const response = await HttpServerTest.request(httpServer, method, url, { ...httpOptions, responseType: 'json' });
+
+        expect(response).toEqual(httpResponse);
+        expect(httpRequestSpy).toHaveBeenNthCalledWith(1, {
+          ...httpOptions,
+          responseType: 'json',
+          method: method,
+          url: `${ httpServerUrl }/${ url }`,
+        });
+      });
+    });
+
+    describe('stream response type', () => {
+      it('should execute Http.request with the correct options and return appropriate data', async () => {
+        const response = await HttpServerTest.request(httpServer, method, url, { ...httpOptions, responseType: 'stream' });
+
+        expect(response).toEqual(httpResponse);
+        expect(httpRequestSpy).toHaveBeenNthCalledWith(1, {
+          ...httpOptions,
+          responseType: 'stream',
+          method: method,
+          url: `${ httpServerUrl }/${ url }`,
+        });
+      });
+    });
+
+    describe('text response type', () => {
+      it('should execute Http.request with the correct options and return appropriate data', async () => {
+        const response = await HttpServerTest.request(httpServer, method, url, { ...httpOptions, responseType: 'text' });
+
+        expect(response).toEqual(httpResponse);
+        expect(httpRequestSpy).toHaveBeenNthCalledWith(1, {
+          ...httpOptions,
+          responseType: 'text',
+          method: method,
+          url: `${ httpServerUrl }/${ url }`,
+        });
+      });
+    });
   });
 });
--- packages/testing/lib/http-server-test.ts
+++ packages/testing/lib/http-server-test.ts
@@ -1,112 +1,28 @@
 import { Http, HttpOptionsBase, HttpResponseBase } from '@caviajs/common';
 import http from 'http';
 import https from 'https';
-import { AddressInfo } from 'net';
+import net from 'net';
 import { Readable } from 'stream';
 
 function composeUrl(server: http.Server | https.Server, url: string): string {
-  const port = (server.address() as AddressInfo).port;
+  const port = (server.address() as net.AddressInfo)?.port;
   const protocol = server instanceof https.Server ? 'https' : 'http';
 
   return `${ protocol }://127.0.0.1:${ port }/${ url.replace(/^\//, '') }`;
 }
 
 export class HttpServerTest {
-  public static delete(server: http.Server | https.Server, url: string, options?: HttpOptions & { responseType?: 'buffer' }): Promise<HttpResponse<Buffer>>;
-  public static delete<T = any>(server: http.Server | https.Server, url: string, options?: HttpOptions & { responseType?: 'json' }): Promise<HttpResponse<T>>;
-  public static delete(server: http.Server | https.Server, url: string, options?: HttpOptions & { responseType?: 'stream' }): Promise<HttpResponse<Readable>>;
-  public static delete(server: http.Server | https.Server, url: string, options?: HttpOptions & { responseType?: 'text' }): Promise<HttpResponse<string>>;
-  public static delete(server: http.Server | https.Server, url: string, options?: HttpOptions): Promise<HttpResponse<any>> {
+  public static request(server: http.Server | https.Server, method: 'DELETE' | 'GET' | 'PATCH' | 'POST' | 'PUT', url: string, options?: HttpOptions & { responseType?: 'buffer' }): Promise<HttpResponse<Buffer>>;
+  public static request<T = any>(server: http.Server | https.Server, method: 'DELETE' | 'GET' | 'PATCH' | 'POST' | 'PUT', url: string, options?: HttpOptions & { responseType?: 'json' }): Promise<HttpResponse<T>>;
+  public static request(server: http.Server | https.Server, method: 'DELETE' | 'GET' | 'PATCH' | 'POST' | 'PUT', url: string, options?: HttpOptions & { responseType?: 'stream' }): Promise<HttpResponse<Readable>>;
+  public static request(server: http.Server | https.Server, method: 'DELETE' | 'GET' | 'PATCH' | 'POST' | 'PUT', url: string, options?: HttpOptions & { responseType?: 'text' }): Promise<HttpResponse<string>>;
+  public static request(server: http.Server | https.Server, method: 'DELETE' | 'GET' | 'PATCH' | 'POST' | 'PUT', url: string, options?: HttpOptions & { responseType?: any }): Promise<HttpResponse<any>> {
     if (server.address() === null) {
       server.listen();
     }
 
     return Http
-      .request({ ...options, method: 'DELETE', url: composeUrl(server, url) })
-      .then(result => {
-        server.close();
-
-        return result;
-      })
-      .catch(error => {
-        throw error;
-      });
-  }
-
-  public static get(server: http.Server | https.Server, url: string, options?: HttpOptions & { responseType?: 'buffer' }): Promise<HttpResponse<Buffer>>;
-  public static get<T = any>(server: http.Server | https.Server, url: string, options?: HttpOptions & { responseType?: 'json' }): Promise<HttpResponse<T>>;
-  public static get(server: http.Server | https.Server, url: string, options?: HttpOptions & { responseType?: 'stream' }): Promise<HttpResponse<Readable>>;
-  public static get(server: http.Server | https.Server, url: string, options?: HttpOptions & { responseType?: 'text' }): Promise<HttpResponse<string>>;
-  public static get(server: http.Server | https.Server, url: string, options?: HttpOptions): Promise<HttpResponse<any>> {
-    if (server.address() === null) {
-      server.listen();
-    }
-
-    return Http
-      .request({ ...options, method: 'GET', url: composeUrl(server, url) })
-      .then(result => {
-        server.close();
-
-        return result;
-      })
-      .catch(error => {
-        throw error;
-      });
-  }
-
-  public static patch(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions & { responseType?: 'buffer' }): Promise<HttpResponse<Buffer>>;
-  public static patch<T = any>(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions & { responseType?: 'json' }): Promise<HttpResponse<T>>;
-  public static patch(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions & { responseType?: 'stream' }): Promise<HttpResponse<Readable>>;
-  public static patch(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions & { responseType?: 'text' }): Promise<HttpResponse<string>>;
-  public static patch(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions): Promise<HttpResponse<any>> {
-    if (server.address() === null) {
-      server.listen();
-    }
-
-    return Http
-      .request({ ...options, body: body, method: 'PATCH', url: composeUrl(server, url) })
-      .then(result => {
-        server.close();
-
-        return result;
-      })
-      .catch(error => {
-        throw error;
-      });
-  }
-
-  public static post(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions & { responseType?: 'buffer' }): Promise<HttpResponse<Buffer>>;
-  public static post<T = any>(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions & { responseType?: 'json' }): Promise<HttpResponse<T>>;
-  public static post(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions & { responseType?: 'stream' }): Promise<HttpResponse<Readable>>;
-  public static post(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions & { responseType?: 'text' }): Promise<HttpResponse<string>>;
-  public static post(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions): Promise<HttpResponse<any>> {
-    if (server.address() === null) {
-      server.listen();
-    }
-
-    return Http
-      .request({ ...options, body: body, method: 'POST', url: composeUrl(server, url) })
-      .then(result => {
-        server.close();
-
-        return result;
-      })
-      .catch(error => {
-        throw error;
-      });
-  }
-
-  public static put(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions & { responseType?: 'buffer' }): Promise<HttpResponse<Buffer>>;
-  public static put<T = any>(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions & { responseType?: 'json' }): Promise<HttpResponse<T>>;
-  public static put(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions & { responseType?: 'stream' }): Promise<HttpResponse<Readable>>;
-  public static put(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions & { responseType?: 'text' }): Promise<HttpResponse<string>>;
-  public static put(server: http.Server | https.Server, url: string, body?: any, options?: HttpOptions): Promise<HttpResponse<any>> {
-    if (server.address() === null) {
-      server.listen();
-    }
-
-    return Http
-      .request({ ...options, body: body, method: 'PUT', url: composeUrl(server, url) })
+      .request({ ...options, method: method, url: composeUrl(server, url) })
       .then(result => {
         server.close();
 
@@ -118,6 +34,6 @@ export class HttpServerTest {
   }
 }
 
-export type HttpOptions = Pick<HttpOptionsBase, 'headers' | 'params' | 'timeout'>;
+export type HttpOptions = Pick<HttpOptionsBase, 'body' | 'headers' | 'params' | 'timeout'>;
 
 export type HttpResponse<T> = HttpResponseBase<T>;
