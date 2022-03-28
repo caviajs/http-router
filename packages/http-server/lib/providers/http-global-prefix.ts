b51f31cba4b96b454af3a79337e8a60f34cd992e
partyka <partyka95@icloud.com>
2022-03-28 18:18:48 +0200

    http global prefix

--- packages/http-server/lib/providers/http-global-prefix.ts
+++ packages/http-server/lib/providers/http-global-prefix.ts
@@ -1,8 +1,5 @@
 import { Token, ValueProvider } from '@caviajs/core';
 
-/**
- * A provider token that represents the global prefix.
- */
 export const HTTP_GLOBAL_PREFIX: Token<HttpGlobalPrefix> = Symbol('HTTP_GLOBAL_PREFIX');
 
 export const HttpGlobalPrefixProvider: ValueProvider<HttpGlobalPrefix> = {
