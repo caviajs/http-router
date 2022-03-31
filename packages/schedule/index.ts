4977804dccbbd544a7767c862a32d4164e7643b9
partyka <partyka95@icloud.com>
2022-03-31 21:42:45 +0200

    improve schedule package

--- packages/schedule/index.ts
+++ packages/schedule/index.ts
@@ -1,1 +1,11 @@
 import 'reflect-metadata';
+
+export * from './lib/adapters/worker';
+
+export * from './lib/decorators/scheduled';
+
+export * from './lib/providers/schedule';
+export * from './lib/providers/schedule-manager';
+
+export * from './lib/schedule-constants';
+export * from './lib/schedule-package';
