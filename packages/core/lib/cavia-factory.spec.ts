40570a59787f8bb13274c97714f5eba6a7ba08d5
partyka <partyka95@icloud.com>
2022-04-03 13:54:31 +0200

    improve core package

--- packages/core/lib/cavia-factory.spec.ts
+++ packages/core/lib/cavia-factory.spec.ts
@@ -1,11 +1,8 @@
+import { Logger, LoggerPackage } from '@caviajs/logger';
 import { Application } from './decorators/application';
 import { CaviaApplication } from './cavia-application';
-import { CaviaApplicationBuilder } from './cavia-application-builder';
 import { CaviaFactory } from './cavia-factory';
-
-@Application()
-class MyApp {
-}
+import { LOGGER_CONTEXT } from './constants';
 
 describe('CaviaFactory', () => {
   afterEach(() => {
@@ -13,15 +10,100 @@ describe('CaviaFactory', () => {
   });
 
   describe('create', () => {
-    it('should use CaviaApplicationBuilder as builder', async () => {
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    it('should throw an exception if the class is not marked with the @Application decorator', async () => {
+      class MyApp {
+      }
+
+      try {
+        await CaviaFactory.create(MyApp);
+      } catch (e) {
+        expect(e.message).toBe(`The '${ MyApp.name }' should be annotated as an application`);
+      }
+    });
+
+    it('should collect application providers', async () => {
+      @Application({
+        packages: [
+          { providers: [{ provide: 'foo-1', useValue: 10 }] },
+          { providers: [{ provide: 'foo-2', useValue: 20 }] },
+        ],
+        providers: [
+          { provide: 'bar-1', useValue: 30 },
+          { provide: 'bar-2', useValue: 40 },
+        ],
+      })
+      class MyApp {
+      }
+
+      const application = await CaviaFactory.create(MyApp);
+
+      expect(await application.injector.find('foo-1')).toEqual(10);
+      expect(await application.injector.find('foo-2')).toEqual(20);
+      expect(await application.injector.find('bar-1')).toEqual(30);
+      expect(await application.injector.find('bar-2')).toEqual(40);
+      expect(await application.injector.find(MyApp)).toBeInstanceOf(MyApp);
+    });
+
+    it('should determine the appropriate weighting of providers', async () => {
+      @Application({
+        packages: [
+          { providers: [{ provide: 'foo', useValue: 1 }] },
+        ],
+        providers: [
+          { provide: 'foo', useValue: 2 },
+          { provide: 'foo', useValue: 3 },
+        ],
+      })
+      class MyApp {
+      }
+
+      const application = await CaviaFactory.create(MyApp);
+
+      expect(await application.injector.find('foo')).toEqual(3);
+    });
+
+    it('should use Logger', async () => {
+      const loggerTraceSpy = jest.spyOn(Logger.prototype, 'trace');
+
+      @Application({
+        packages: [LoggerPackage.configure().register()],
+      })
+      class MyApp {
+      }
+
+      expect(loggerTraceSpy).toHaveBeenCalledTimes(0);
+
       await CaviaFactory.create(MyApp);
 
-      expect(CaviaApplicationBuilder).toHaveBeenNthCalledWith(1, MyApp);
-      expect(CaviaApplicationBuilder.prototype.compile).toHaveBeenCalledTimes(1);
+      expect(loggerTraceSpy).toHaveBeenNthCalledWith(1, 'Starting application...', LOGGER_CONTEXT);
+    });
+
+    it('should call a boot method on the CaviaApplication instance', async () => {
+      const caviaApplicationBootSpy = jest.spyOn(CaviaApplication.prototype, 'boot');
+
+      @Application()
+      class MyApp {
+      }
+
+      expect(caviaApplicationBootSpy).toHaveBeenCalledTimes(0);
+
+      await CaviaFactory.create(MyApp);
+
+      expect(caviaApplicationBootSpy).toHaveBeenNthCalledWith(1);
     });
 
     it('should return CaviaApplication instance', async () => {
-      expect(await CaviaFactory.create(MyApp)).toBeInstanceOf(CaviaApplication);
+      @Application()
+      class MyApp {
+      }
+
+      const application = await CaviaFactory.create(MyApp);
+
+      expect(application).toBeInstanceOf(CaviaApplication);
     });
   });
 });
--- packages/core/lib/cavia-factory.ts
+++ packages/core/lib/cavia-factory.ts
@@ -1,10 +1,45 @@
+import { Logger } from '@caviajs/logger';
+import { APPLICATION_METADATA, ApplicationMetadata } from './decorators/application';
+import { Provider } from './types/provider';
+import { Token } from './types/token';
 import { Type } from './types/type';
+import { getProviderToken } from './utils/get-provider-token';
 import { CaviaApplication } from './cavia-application';
-import { CaviaApplicationBuilder } from './cavia-application-builder';
+import { LOGGER_CONTEXT } from './constants';
+import { Injector } from './injector';
 
 export class CaviaFactory {
-  public static create(application: Type): Promise<CaviaApplication> {
-    return new CaviaApplicationBuilder(application)
-      .compile();
+  public static async create(application: Type): Promise<CaviaApplication> {
+    if (Reflect.hasMetadata(APPLICATION_METADATA, application) === false) {
+      throw new Error(`The '${ application?.name }' should be annotated as an application`);
+    }
+
+    const injector: Injector = await Injector.create([...this.getApplicationProviders(application).values()]);
+    const caviaApplication: CaviaApplication = new CaviaApplication(injector);
+
+    (await injector.find(Logger))?.trace('Starting application...', LOGGER_CONTEXT);
+
+    await caviaApplication.boot();
+
+    return caviaApplication;
+  }
+
+  protected static getApplicationProviders(application: Type): Map<Token, Provider> {
+    const applicationMetadata: ApplicationMetadata = Reflect.getMetadata(APPLICATION_METADATA, application);
+
+    const providers: Map<Token, Provider> = new Map();
+
+    for (const pkg of [...applicationMetadata?.packages || []]) {
+      for (const provider of pkg.providers || []) {
+        providers.set(getProviderToken(provider), provider);
+      }
+    }
+
+    // todo { provide: APPLICATION_REF, useExisting: application } ?
+    for (const provider of [...applicationMetadata?.providers || [], application]) {
+      providers.set(getProviderToken(provider), provider);
+    }
+
+    return providers;
   }
 }
