e84ac79adda6e85e70231a08918db91c5f318b7a
partyka <partyka95@icloud.com>
2022-04-06 17:27:28 +0200

    logger spec

--- packages/logger/lib/providers/logger.spec.ts
+++ packages/logger/lib/providers/logger.spec.ts
@@ -16,7 +16,7 @@ describe('Logger', () => {
   describe('LoggerLevel.OFF', () => {
     const logger: Logger = new Logger(LoggerLevel.OFF, loggerMessageFactorySpy);
 
-    it('should not call process.stdout.write for the appropriate levels', () => {
+    it('should log the appropriate levels', () => {
       logger.fatal(message);
       logger.error(message);
       logger.warn(message);
@@ -24,6 +24,19 @@ describe('Logger', () => {
       logger.debug(message);
       logger.trace(message);
 
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalled();
+      expect(writeSpy).not.toHaveBeenCalled();
+    });
+
+    it('should log the appropriate levels with context', () => {
+      logger.fatal(message, context);
+      logger.error(message, context);
+      logger.warn(message, context);
+      logger.info(message, context);
+      logger.debug(message, context);
+      logger.trace(message, context);
+
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalled();
       expect(writeSpy).not.toHaveBeenCalled();
     });
   });
@@ -31,154 +44,173 @@ describe('Logger', () => {
   describe('LoggerLevel.FATAL', () => {
     const logger: Logger = new Logger(LoggerLevel.FATAL, loggerMessageFactorySpy);
 
-    it('should call process.stdout.write for the appropriate levels', () => {
+    it('should log the appropriate levels', () => {
       logger.fatal(message);
-
-      expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(1);
-      expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.FATAL, message: message });
-      expect(writeSpy).toHaveBeenNthCalledWith(1, message);
-    });
-
-    it('should call process.stdout.write for the appropriate levels with context', () => {
-      logger.fatal(message, context);
-
-      expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(1);
-      expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.FATAL, message: message });
-      expect(writeSpy).toHaveBeenNthCalledWith(1, message);
-    });
-
-    it('should not call process.stdout.write for the appropriate levels', () => {
       logger.error(message);
       logger.warn(message);
       logger.info(message);
       logger.debug(message);
       logger.trace(message);
 
-      expect(loggerMessageFactorySpy).not.toHaveBeenCalled();
-      expect(writeSpy).not.toHaveBeenCalled();
+      expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(1);
+      expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.FATAL, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.ERROR, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.WARN, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.INFO, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.DEBUG, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.TRACE, message: message });
+      expect(writeSpy).toHaveBeenNthCalledWith(1, message);
+    });
+
+    it('should log the appropriate levels with context', () => {
+      logger.fatal(message, context);
+      logger.error(message, context);
+      logger.warn(message, context);
+      logger.info(message, context);
+      logger.debug(message, context);
+      logger.trace(message, context);
+
+      expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(1);
+      expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.FATAL, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.ERROR, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.WARN, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.INFO, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.DEBUG, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.TRACE, message: message });
+      expect(writeSpy).toHaveBeenNthCalledWith(1, message);
     });
   });
 
   describe('LoggerLevel.ERROR', () => {
     const logger: Logger = new Logger(LoggerLevel.ERROR, loggerMessageFactorySpy);
 
-    it('should call process.stdout.write for the appropriate levels', () => {
+    it('should log the appropriate levels', () => {
       logger.fatal(message);
       logger.error(message);
-
-      expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(2);
-      expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.FATAL, message: message });
-      expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.ERROR, message: message });
-      expect(writeSpy).toHaveBeenNthCalledWith(2, message);
-    });
-
-    it('should call process.stdout.write for the appropriate levels with context', () => {
-      logger.fatal(message, context);
-      logger.error(message, context);
-
-      expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(2);
-      expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.FATAL, message: message });
-      expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.ERROR, message: message });
-      expect(writeSpy).toHaveBeenNthCalledWith(2, message);
-    });
-
-    it('should not call process.stdout.write for the appropriate levels', () => {
       logger.warn(message);
       logger.info(message);
       logger.debug(message);
       logger.trace(message);
 
-      expect(loggerMessageFactorySpy).not.toHaveBeenCalled();
-      expect(writeSpy).not.toHaveBeenCalled();
+      expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(2);
+      expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.FATAL, message: message });
+      expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.ERROR, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.WARN, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.INFO, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.DEBUG, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.TRACE, message: message });
+      expect(writeSpy).toHaveBeenNthCalledWith(2, message);
+    });
+
+    it('should log the appropriate levels with context', () => {
+      logger.fatal(message, context);
+      logger.error(message, context);
+      logger.warn(message, context);
+      logger.info(message, context);
+      logger.debug(message, context);
+      logger.trace(message, context);
+
+      expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(2);
+      expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.FATAL, message: message });
+      expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.ERROR, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.WARN, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.INFO, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.DEBUG, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.TRACE, message: message });
+      expect(writeSpy).toHaveBeenNthCalledWith(2, message);
     });
   });
 
   describe('LoggerLevel.WARN', () => {
     const logger: Logger = new Logger(LoggerLevel.WARN, loggerMessageFactorySpy);
 
-    it('should call process.stdout.write for the appropriate levels', () => {
+    it('should log the appropriate levels', () => {
       logger.fatal(message);
       logger.error(message);
       logger.warn(message);
+      logger.info(message);
+      logger.debug(message);
+      logger.trace(message);
 
       expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(3);
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.FATAL, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.ERROR, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.WARN, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.INFO, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.DEBUG, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.TRACE, message: message });
       expect(writeSpy).toHaveBeenNthCalledWith(3, message);
     });
 
-    it('should call process.stdout.write for the appropriate levels with context', () => {
+    it('should log the appropriate levels with context', () => {
       logger.fatal(message, context);
       logger.error(message, context);
       logger.warn(message, context);
+      logger.info(message, context);
+      logger.debug(message, context);
+      logger.trace(message, context);
 
       expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(3);
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.FATAL, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.ERROR, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.WARN, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.INFO, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.DEBUG, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.TRACE, message: message });
       expect(writeSpy).toHaveBeenNthCalledWith(3, message);
-    });
-
-    it('should not call process.stdout.write for the appropriate levels', () => {
-      logger.info(message);
-      logger.debug(message);
-      logger.trace(message);
-
-      expect(loggerMessageFactorySpy).not.toHaveBeenCalled();
-      expect(writeSpy).not.toHaveBeenCalled();
     });
   });
 
   describe('LoggerLevel.INFO', () => {
     const logger: Logger = new Logger(LoggerLevel.INFO, loggerMessageFactorySpy);
 
-    it('should call process.stdout.write for the appropriate levels', () => {
+    it('should log the appropriate levels', () => {
       logger.fatal(message);
       logger.error(message);
       logger.warn(message);
       logger.info(message);
+      logger.debug(message);
+      logger.trace(message);
 
       expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(4);
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.FATAL, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.ERROR, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.WARN, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.INFO, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.DEBUG, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.TRACE, message: message });
       expect(writeSpy).toHaveBeenNthCalledWith(4, message);
     });
 
-    it('should call process.stdout.write for the appropriate levels with context', () => {
+    it('should log the appropriate levels with context', () => {
       logger.fatal(message, context);
       logger.error(message, context);
       logger.warn(message, context);
       logger.info(message, context);
+      logger.debug(message, context);
+      logger.trace(message, context);
 
       expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(4);
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.FATAL, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.ERROR, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.WARN, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.INFO, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.DEBUG, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.TRACE, message: message });
       expect(writeSpy).toHaveBeenNthCalledWith(4, message);
-    });
-
-    it('should not call process.stdout.write for the appropriate levels', () => {
-      logger.debug(message);
-      logger.trace(message);
-
-      expect(loggerMessageFactorySpy).not.toHaveBeenCalled();
-      expect(writeSpy).not.toHaveBeenCalled();
     });
   });
 
   describe('LoggerLevel.DEBUG', () => {
     const logger: Logger = new Logger(LoggerLevel.DEBUG, loggerMessageFactorySpy);
 
-    it('should call process.stdout.write for the appropriate levels', () => {
+    it('should log the appropriate levels', () => {
       logger.fatal(message);
       logger.error(message);
       logger.warn(message);
       logger.info(message);
       logger.debug(message);
+      logger.trace(message);
 
       expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(5);
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.FATAL, message: message });
@@ -186,15 +218,17 @@ describe('Logger', () => {
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.WARN, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.INFO, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.DEBUG, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: undefined, level: LoggerLevel.TRACE, message: message });
       expect(writeSpy).toHaveBeenNthCalledWith(5, message);
     });
 
-    it('should call process.stdout.write for the appropriate levels with context', () => {
+    it('should log the appropriate levels with context', () => {
       logger.fatal(message, context);
       logger.error(message, context);
       logger.warn(message, context);
       logger.info(message, context);
       logger.debug(message, context);
+      logger.trace(message, context);
 
       expect(loggerMessageFactorySpy).toHaveBeenCalledTimes(5);
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.FATAL, message: message });
@@ -202,21 +236,15 @@ describe('Logger', () => {
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.WARN, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.INFO, message: message });
       expect(loggerMessageFactorySpy).toHaveBeenCalledWith({ context: context, level: LoggerLevel.DEBUG, message: message });
+      expect(loggerMessageFactorySpy).not.toHaveBeenCalledWith({ context: context, level: LoggerLevel.TRACE, message: message });
       expect(writeSpy).toHaveBeenNthCalledWith(5, message);
-    });
-
-    it('should not call process.stdout.write for the appropriate levels', () => {
-      logger.trace(message);
-
-      expect(loggerMessageFactorySpy).not.toHaveBeenCalled();
-      expect(writeSpy).not.toHaveBeenCalled();
     });
   });
 
   describe('LoggerLevel.TRACE', () => {
     const logger: Logger = new Logger(LoggerLevel.TRACE, loggerMessageFactorySpy);
 
-    it('should call process.stdout.write for the appropriate levels', () => {
+    it('should log the appropriate levels', () => {
       logger.fatal(message);
       logger.error(message);
       logger.warn(message);
@@ -234,7 +262,7 @@ describe('Logger', () => {
       expect(writeSpy).toHaveBeenNthCalledWith(6, message);
     });
 
-    it('should call process.stdout.write for the appropriate levels with context', () => {
+    it('should log the appropriate levels with context', () => {
       logger.fatal(message, context);
       logger.error(message, context);
       logger.warn(message, context);
@@ -256,7 +284,7 @@ describe('Logger', () => {
   describe('LoggerLevel.ALL', () => {
     const logger: Logger = new Logger(LoggerLevel.ALL, loggerMessageFactorySpy);
 
-    it('should call process.stdout.write for the appropriate levels', () => {
+    it('should log the appropriate levels', () => {
       logger.fatal(message);
       logger.error(message);
       logger.warn(message);
@@ -274,7 +302,7 @@ describe('Logger', () => {
       expect(writeSpy).toHaveBeenNthCalledWith(6, message);
     });
 
-    it('should call process.stdout.write for the appropriate levels with context', () => {
+    it('should log the appropriate levels with context', () => {
       logger.fatal(message, context);
       logger.error(message, context);
       logger.warn(message, context);
