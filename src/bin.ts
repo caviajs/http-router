8157830dd2d3d75e54e0470cd975a2e7b9a0c2ad
partyka <partyka95@icloud.com>
2022-04-19 00:51:46 +0200

    bin fix

--- src/bin.ts
+++ src/bin.ts
@@ -34,14 +34,19 @@ yargs
       const name: string = (argv.name as string).toLowerCase();
       const path: string[] = argv.path ? (argv.path as string).split(sep) : [];
 
-      const dist = join(process.cwd(), ...path, `${ name }.${ template }.ts`);
+      const distPath = join(process.cwd(), ...path);
+      const dist = join(distPath, `${ name }.${ template }.ts`);
       const data = await edge.render(template, {
         name: name[0].toUpperCase() + name.slice(1),
       });
 
+      if (!fs.existsSync(distPath)) {
+        fs.mkdirSync(distPath);
+      }
+
       fs.writeFileSync(dist, data);
 
-      process.stdout.write(`File '${ chalk.magentaBright(dist) } has been generated'\n`);
+      process.stdout.write(`File '${ chalk.magentaBright(dist) }' has been generated\n`);
     },
   })
   .parse();
