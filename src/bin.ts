9174fa49e8dc8a49233cd9443ad80d4d51103edc
partyka <partyka95@icloud.com>
2022-04-18 19:52:26 +0200

    bin

--- src/bin.ts
+++ src/bin.ts
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
-import 'reflect-metadata';
-
 import yargs from 'yargs';
 
-yargs
-  .version(require('./package.json').version)
-  .scriptName('cavia')
-  .command({
-    command: [
-      'oasgc',
-    ],
-    handler: async args => {
-      console.log('oasgc');
-    },
-  })
-  .parse();
+import { Edge } from 'edge.js';
+import path from 'path';
+import fs from 'fs';
+
+yargs.command('generate', 'generate cavia component', args => args, async argv => {
+  console.log('start:file generated');
+  const name = 'siema';
+
+  const edge = new Edge();
+  edge.mount(path.join(__dirname, 'bin', 'templates'));
+  const data = await edge.render('controller', { name: name });
+
+  fs.writeFileSync(path.join(process.cwd(), 'src', 'controllers', `${ name }.controller.ts`), data);
+  console.log('end:file generated');
+});
+
+yargs.parse();
