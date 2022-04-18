9d6f21227c599912e669551bb8ca82f6038335c7
partyka <partyka95@icloud.com>
2022-04-18 22:49:35 +0200

    cli

--- src/bin.ts
+++ src/bin.ts
@@ -2,19 +2,46 @@
 import yargs from 'yargs';
 
 import { Edge } from 'edge.js';
-import path from 'path';
-import fs from 'fs-extra';
+import { join, sep } from 'path';
+import fs from 'fs';
+import clc from 'cli-color';
 
 yargs
-  .command('generate', 'generate cavia component', args => args, async argv => {
-    console.log('start:file generated');
-    const name = 'siema';
+  .command({
+    command: 'generate <template> <name>',
+    describe: 'generate cavia component',
+    builder: args => {
+      return args
+        .positional('template', {
+          choices: ['controller', 'interceptor', 'service'],
+          demandOption: false,
+          type: 'string',
+        })
+        .positional('name', {
+          demandOption: false,
+          type: 'string',
+        })
+        .option('path', {
+          demandOption: true,
+          type: 'string',
+          default: '',
+        });
+    },
+    handler: async argv => {
+      const edge: Edge = new Edge().mount(join(__dirname, 'bin', 'templates'));
 
-    const edge = new Edge();
-    edge.mount(path.join(__dirname, 'bin', 'templates'));
-    const data = await edge.render('controller', { name: name });
+      const template: string = argv.template as string;
+      const name: string = (argv.name as string).toLowerCase();
+      const path: string[] = argv.path ? (argv.path as string).split(sep) : [];
 
-    await fs.writeFile(path.join(process.cwd(), 'src', 'controllers', `${ name }.controller.ts`), data);
-    console.log('end:file generated: ', path.join(process.cwd(), 'src', 'controllers', `${ name }.controller.ts`));
+      const dist = join(process.cwd(), ...path, `${ name }.${ template }.ts`);
+      const data = await edge.render(template, {
+        name: name[0].toUpperCase() + name.slice(1),
+      });
+
+      fs.writeFileSync(dist, data);
+
+      process.stdout.write(`File '${ clc.magentaBright(dist) } has been generated'\n`);
+    },
   })
   .parse();
