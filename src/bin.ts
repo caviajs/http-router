ef22bd59552fbc4f482c8f30c925f370ce66fb69
partyka <partyka95@icloud.com>
2022-04-18 20:00:10 +0200

    bin

--- src/bin.ts
+++ src/bin.ts
@@ -3,18 +3,18 @@ import yargs from 'yargs';
 
 import { Edge } from 'edge.js';
 import path from 'path';
-import fs from 'fs';
+import fs from 'fs-extra';
 
-yargs.command('generate', 'generate cavia component', args => args, async argv => {
-  console.log('start:file generated');
-  const name = 'siema';
+yargs
+  .command('generate', 'generate cavia component', args => args, async argv => {
+    console.log('start:file generated');
+    const name = 'siema';
 
-  const edge = new Edge();
-  edge.mount(path.join(__dirname, 'bin', 'templates'));
-  const data = await edge.render('controller', { name: name });
+    const edge = new Edge();
+    edge.mount(path.join(__dirname, 'bin', 'templates'));
+    const data = await edge.render('controller', { name: name });
 
-  fs.writeFileSync(path.join(process.cwd(), 'src', 'controllers', `${ name }.controller.ts`), data);
-  console.log('end:file generated');
-});
-
-yargs.parse();
+    await fs.writeFile(path.join(process.cwd(), 'src', 'controllers', `${ name }.controller.ts`), data);
+    console.log('end:file generated: ', path.join(process.cwd(), 'src', 'controllers', `${ name }.controller.ts`));
+  })
+  .parse();
