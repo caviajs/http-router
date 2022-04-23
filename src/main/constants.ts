39f29fe5f1950a954862e64838bb4827e0d23175
partyka <partyka95@icloud.com>
2022-04-23 20:19:15 +0200

    constants

--- src/main/constants.ts
+++ src/main/constants.ts
@@ -1,3 +1,8 @@
+/** Core **/
 export const CORE_CONTEXT: string = 'Core';
+
+/** Http **/
 export const HTTP_CONTEXT: string = 'Http';
+
+/** Schedule **/
 export const SCHEDULE_CONTEXT: string = 'Schedule';
