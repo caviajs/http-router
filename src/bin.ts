70f17d727affc663f32b77e4246e5810a4c1f5e6
partyka <partyka95@icloud.com>
2022-05-07 13:25:57 +0200

    improve cavia cli

--- src/bin.ts
+++ src/bin.ts
@@ -6,8 +6,16 @@ import fs from 'fs';
 import chalk from 'chalk';
 import { pascalCase } from './main/utils/pascal-case';
 import { kebabCase } from './main/utils/kebab-case';
+import { ApiSpec } from './main/providers/http-server-router';
+import { camelCase } from './main/utils/camel-case';
+import { snakeCase } from './main/utils/snake-case';
 
 const edge: Edge = new Edge().mount(join(__dirname, 'bin', 'templates'));
+
+edge.global('camelCase', (data: string) => camelCase(data));
+edge.global('kebabCase', (data: string) => kebabCase(data));
+edge.global('pascalCase', (data: string) => pascalCase(data));
+edge.global('snakeCase', (data: string) => snakeCase(data));
 
 async function generate(options: { template: string, path: string }): Promise<void> {
   const paths: string[] = options.path.replace(/(\/|\\)/g, sep).split(sep);
@@ -28,7 +36,75 @@ async function generate(options: { template: string, path: string }): Promise<vo
   process.stdout.write(`File '${ chalk.blueBright(dist) }' has been generated\n`);
 }
 
+async function generateHttpClient(options: { path: string, spec: string }): Promise<void> {
+  const apiSpec: ApiSpec = {
+    endpoints: [
+      {
+        'name': 'UpdateUserEndpoint',
+        'data': {
+          'permissions': 'users:update'
+        },
+        'method': 'PUT',
+        'path': '/users/:id',
+        'schema': {
+          'request': {
+            'body': {
+              'type': 'string',
+              'required': true
+            },
+            'headers': {
+              'properties': {
+                'x-test': {
+                  'type': 'string',
+                  'required': true
+                }
+              },
+              'type': 'object'
+            },
+            'query': {
+              'properties': {
+                'lorem': {
+                  'type': 'string',
+                  'required': true
+                }
+              },
+              'type': 'object'
+            }
+          }
+        }
+      },
+    ],
+  };
+
+  const paths: string[] = options.path.replace(/(\/|\\)/g, sep).split(sep);
+  const distDir: string = join(process.cwd(), ...paths.slice(0, -1));
+  const componentNameAsKebabCase: string = kebabCase(paths[paths.length - 1]);
+  const componentNameAsPascalCase: string = pascalCase(paths[paths.length - 1]);
+  const dist: string = join(distDir, `${ componentNameAsKebabCase }.http-client.ts`);
+
+  if (!fs.existsSync(distDir)) {
+    fs.mkdirSync(distDir);
+  }
+
+  fs.writeFileSync(dist, await edge.render('http-client', {
+    endpoints: apiSpec.endpoints,
+    name: componentNameAsPascalCase,
+  }));
+
+  process.stdout.write(`File '${ chalk.blueBright(dist) }' has been generated\n`);
+}
+
 yargs
+  .command({
+    command: 'generate-http-client <path>',
+    describe: 'generate http client',
+    builder: args => {
+      return args
+        .positional('path', { demandOption: false, type: 'string' })
+        .positional('spec', { demandOption: false, type: 'string' });
+    },
+    handler: args => generateHttpClient({ path: args.path as string, spec: args.path as string }),
+  })
   .command({
     command: 'make:endpoint <path>',
     describe: 'generate endpoint',
