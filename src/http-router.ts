6e381d6d3a3ff235e21d706df83269adef31664a
partyka <partyka95@icloud.com>
2022-05-17 23:15:12 +0200

    fix: http router

--- src/http-router.ts
+++ src/http-router.ts
@@ -130,38 +130,29 @@ export class HttpRouter {
   protected serialize(response: http.ServerResponse, data: any): void {
     if (data === undefined) {
       response
-        .writeHead(response.statusCode || 204)
         .end();
     } else if (Buffer.isBuffer(data)) {
       response
-        .writeHead(response.statusCode || 200, {
-          'Content-Length': response.getHeader('Content-Length') || data.length,
-          'Content-Type': response.getHeader('Content-Type') || 'application/octet-stream',
-        })
+        .setHeader('Content-Length', response.getHeader('Content-Length') || data.length)
+        .setHeader('Content-Type', response.getHeader('Content-Type') || 'application/octet-stream')
         .end(data);
     } else if (data instanceof Stream || readable(data)) {
       response
-        .writeHead(response.statusCode || 200, {
-          'Content-Type': response.getHeader('Content-Type') || 'application/octet-stream',
-        });
+        .setHeader('Content-Type', response.getHeader('Content-Type') || 'application/octet-stream');
 
       data.pipe(response);
     } else if (typeof data === 'string') {
       response
-        .writeHead(response.statusCode || 200, {
-          'Content-Length': response.getHeader('Content-Length') || Buffer.byteLength(data),
-          'Content-Type': response.getHeader('Content-Type') || 'text/plain',
-        })
+        .setHeader('Content-Length', response.getHeader('Content-Length') || Buffer.byteLength(data))
+        .setHeader('Content-Type', response.getHeader('Content-Type') || 'text/plain')
         .end(data);
     } else if (typeof data === 'boolean' || typeof data === 'number' || typeof data === 'object') {
       // JSON (true, false, number, null, array, object) but without string
       const raw: string = JSON.stringify(data);
 
       response
-        .writeHead(response.statusCode || 200, {
-          'Content-Length': response.getHeader('Content-Length') || Buffer.byteLength(raw),
-          'Content-Type': response.getHeader('Content-Type') || 'application/json; charset=utf-8',
-        })
+        .setHeader('Content-Length', response.getHeader('Content-Length') || Buffer.byteLength(raw))
+        .setHeader('Content-Type', response.getHeader('Content-Type') || 'application/json; charset=utf-8')
         .end(raw);
     }
   }
