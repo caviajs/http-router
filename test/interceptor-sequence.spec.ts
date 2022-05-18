1a1ebe9ff4da57b8b3d453641f495f10236c7c26
partyka <partyka95@icloud.com>
2022-05-18 19:26:20 +0200

    test: improve

--- test/interceptor-sequence.spec.ts
+++ test/interceptor-sequence.spec.ts
@@ -12,59 +12,57 @@ function wait(ms: number, cb: () => void): Promise<void> {
   });
 }
 
-describe('Interceptor sequence', () => {
-  it('should be executed in the correct order', async () => {
-    const sequence: number[] = [];
+it('should execute the interceptors in the correct sequence', async () => {
+  const sequence: number[] = [];
 
-    const httpRouter: HttpRouter = new HttpRouter();
+  const httpRouter: HttpRouter = new HttpRouter();
 
-    httpRouter
-      // sync
-      .intercept((request, response, next) => {
-        sequence.push(10);
+  httpRouter
+    // sync
+    .intercept((request, response, next) => {
+      sequence.push(10);
 
-        return next.handle().pipe(tap(() => sequence.push(90)));
-      })
-      // async
-      .intercept(async (request, response, next) => {
-        sequence.push(20);
-        await wait(500, () => sequence.push(21));
+      return next.handle().pipe(tap(() => sequence.push(90)));
+    })
+    // async
+    .intercept(async (request, response, next) => {
+      sequence.push(20);
+      await wait(500, () => sequence.push(21));
 
-        return next.handle().pipe(tap(() => sequence.push(80)));
-      });
-
-    httpRouter
-      .route({
-        handler: async () => {
-          sequence.push(50);
-          await wait(500, () => sequence.push(51));
-          sequence.push(52);
-        },
-        interceptors: [
-          // sync
-          (request, response, next) => {
-            sequence.push(30);
-
-            return next.handle().pipe(tap(() => sequence.push(70)));
-          },
-          // async
-          async (request, response, next) => {
-            sequence.push(40);
-            await wait(500, () => sequence.push(41));
-
-            return next.handle().pipe(tap(() => sequence.push(60)));
-          },
-        ],
-        method: 'GET',
-        path: '/pigs'
-      });
-
-    const httpServer: http.Server = http.createServer((request, response) => {
-      httpRouter.handle(request, response);
+      return next.handle().pipe(tap(() => sequence.push(80)));
     });
 
-    await supertest(httpServer).get('/pigs');
+  httpRouter
+    .route({
+      handler: async () => {
+        sequence.push(50);
+        await wait(500, () => sequence.push(51));
+        sequence.push(52);
+      },
+      interceptors: [
+        // sync
+        (request, response, next) => {
+          sequence.push(30);
 
-    expect(sequence).toEqual([10, 20, 21, 30, 40, 41, 50, 51, 52, 60, 70, 80, 90]);
+          return next.handle().pipe(tap(() => sequence.push(70)));
+        },
+        // async
+        async (request, response, next) => {
+          sequence.push(40);
+          await wait(500, () => sequence.push(41));
+
+          return next.handle().pipe(tap(() => sequence.push(60)));
+        },
+      ],
+      method: 'GET',
+      path: '/pigs'
+    });
+
+  const httpServer: http.Server = http.createServer((request, response) => {
+    httpRouter.handle(request, response);
   });
+
+  await supertest(httpServer).get('/pigs');
+
+  expect(sequence).toEqual([10, 20, 21, 30, 40, 41, 50, 51, 52, 60, 70, 80, 90]);
 });
