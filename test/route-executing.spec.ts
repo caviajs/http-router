24d004789c6f8e51c1aea2010979948a5a951473
partyka <partyka95@icloud.com>
2022-05-18 22:20:09 +0200

    test: route executing

--- test/route-executing.spec.ts
+++ test/route-executing.spec.ts
@@ -6,38 +6,44 @@ it('should execute the handler of the appropriate route', (done) => {
   const httpRouter: HttpRouter = new HttpRouter();
 
   httpRouter
+    .route({ handler: () => 'DELETE /pigs/:id', method: 'DELETE', path: '/pigs/:id' })
     .route({ handler: () => 'GET /pigs', method: 'GET', path: '/pigs' })
+    .route({ handler: () => 'GET /pigs/:id', method: 'GET', path: '/pigs/:id' })
+    .route({ handler: () => 'PATCH /pigs/:id', method: 'PATCH', path: '/pigs/:id' })
     .route({ handler: () => 'POST /pigs', method: 'POST', path: '/pigs' })
-    .route({ handler: (request) => `GET /pigs/${ request.params.id }`, method: 'GET', path: '/pigs/:id' })
-    .route({ handler: (request) => `PUT /pigs/${ request.params.id }`, method: 'PUT', path: '/pigs/:id' })
-    .route({ handler: (request) => `DELETE /pigs/${ request.params.id }`, method: 'DELETE', path: '/pigs/:id' });
+    .route({ handler: () => 'PUT /pigs/:id', method: 'PUT', path: '/pigs/:id' });
 
   const httpServer: http.Server = http.createServer((request, response) => {
     httpRouter.handle(request, response);
   });
+
+  // DELETE /pigs/:id
+  supertest(httpServer)
+    .delete('/pigs/1')
+    .expect(200, 'DELETE /pigs/:id', done);
 
   // GET /pigs
   supertest(httpServer)
     .get('/pigs')
     .expect(200, 'GET /pigs', done);
 
+  // GET /pigs/:id
+  supertest(httpServer)
+    .get('/pigs/1')
+    .expect(200, 'GET /pigs/:id', done);
+
+  // PATCH /pigs/:id
+  supertest(httpServer)
+    .patch('/pigs/1')
+    .expect(200, 'PATCH /pigs/:id', done);
+
   // POST /pigs
   supertest(httpServer)
     .post('/pigs')
     .expect(200, 'POST /pigs', done);
 
-  // GET /pigs/:id
-  supertest(httpServer)
-    .get('/pigs/1245')
-    .expect(200, 'GET /pigs/1245', done);
-
   // PUT /pigs/:id
   supertest(httpServer)
-    .put('/pigs/4578')
-    .expect(200, 'PUT /pigs/4578', done);
-
-  // DELETE /pigs/:id
-  supertest(httpServer)
-    .delete('/pigs/40')
-    .expect(200, 'DELETE /pigs/40', done);
+    .put('/pigs/1')
+    .expect(200, 'PUT /pigs/:id', done);
 });
