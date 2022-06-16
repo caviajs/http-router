cf94dae35cc2503f5f5f44b1ecd78c61e0be4595
Paweł Partyka <partyka95@icloud.com>
2022-06-16 16:14:54 +0200

    test: route serialize buffer

--- test/route-serialize-buffer.spec.ts
+++ test/route-serialize-buffer.spec.ts
@@ -89,7 +89,7 @@ it('should correctly overwrite headers after buffer serialization, if specified 
   httpRouter.route({
     handler: (request, response) => {
       response
-        .setHeader('content-length', '4')
+        .setHeader('content-length', '11')
         .setHeader('content-type', 'application/javascript');
 
       return EXAMPLE_BUFFER;
@@ -105,6 +105,6 @@ it('should correctly overwrite headers after buffer serialization, if specified 
   const response = await supertest(httpServer)
     .get('/');
 
-  expect(response.headers['content-length']).toBe('4');
+  expect(response.headers['content-length']).toBe('11');
   expect(response.headers['content-type']).toBe('application/javascript');
 });
