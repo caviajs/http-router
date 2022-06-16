9aee5fdb20962a30b1e9d04d2a4ba1ccbaab93a9
Paweł Partyka <partyka95@icloud.com>
2022-06-16 16:39:08 +0200

    test: response serialization

--- test/interceptor-serialize-null.spec.ts
+++ test/interceptor-serialize-null.spec.ts
@@ -5,121 +5,14 @@ import { HttpRouter } from '../src';
 
 const EXAMPLE_NULL: boolean = false;
 
-describe('interceptor serialize null', () => {
-  it('should correctly serialize null returned by interceptor', async () => {
-    // sync
-    {
-      const httpRouter: HttpRouter = new HttpRouter();
-
-      httpRouter
-        .intercept((request, response, next) => {
-          return next.handle().pipe(map(() => EXAMPLE_NULL));
-        })
-        .route({ handler: () => undefined, method: 'GET', path: '/' });
-
-      const httpServer: http.Server = http.createServer((request, response) => {
-        httpRouter.handle(request, response);
-      });
-
-      const raw: string = JSON.stringify(EXAMPLE_NULL);
-
-      const response = await supertest(httpServer)
-        .get('/');
-
-      expect(response.body).toEqual(EXAMPLE_NULL);
-      expect(response.headers['content-length']).toBe(Buffer.byteLength(raw).toString());
-      expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
-      expect(response.statusCode).toBe(200);
-    }
-
-    // async
-    {
-      const httpRouter: HttpRouter = new HttpRouter();
-
-      httpRouter
-        .intercept((request, response, next) => {
-          return next.handle().pipe(exhaustMap(() => Promise.resolve(EXAMPLE_NULL)));
-        })
-        .route({ handler: () => undefined, method: 'GET', path: '/' });
-
-      const httpServer: http.Server = http.createServer((request, response) => {
-        httpRouter.handle(request, response);
-      });
-
-      const raw: string = JSON.stringify(EXAMPLE_NULL);
-
-      const response = await supertest(httpServer)
-        .get('/');
-
-      expect(response.body).toEqual(EXAMPLE_NULL);
-      expect(response.headers['content-length']).toBe(Buffer.byteLength(raw).toString());
-      expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
-      expect(response.statusCode).toBe(200);
-    }
-
-    // sync + observable
-    {
-      const httpRouter: HttpRouter = new HttpRouter();
-
-      httpRouter
-        .intercept((request, response, next) => {
-          return next.handle().pipe(exhaustMap(() => of(EXAMPLE_NULL)));
-        })
-        .route({ handler: () => undefined, method: 'GET', path: '/' });
-
-      const httpServer: http.Server = http.createServer((request, response) => {
-        httpRouter.handle(request, response);
-      });
-
-      const raw: string = JSON.stringify(EXAMPLE_NULL);
-
-      const response = await supertest(httpServer)
-        .get('/');
-
-      expect(response.body).toEqual(EXAMPLE_NULL);
-      expect(response.headers['content-length']).toBe(Buffer.byteLength(raw).toString());
-      expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
-      expect(response.statusCode).toBe(200);
-    }
-
-    // async + observable
-    {
-      const httpRouter: HttpRouter = new HttpRouter();
-
-      httpRouter
-        .intercept((request, response, next) => {
-          return next.handle().pipe(exhaustMap(() => Promise.resolve(of(EXAMPLE_NULL))), mergeAll());
-        })
-        .route({ handler: () => undefined, method: 'GET', path: '/' });
-
-      const httpServer: http.Server = http.createServer((request, response) => {
-        httpRouter.handle(request, response);
-      });
-
-      const raw: string = JSON.stringify(EXAMPLE_NULL);
-
-      const response = await supertest(httpServer)
-        .get('/');
-
-      expect(response.body).toEqual(EXAMPLE_NULL);
-      expect(response.headers['content-length']).toBe(Buffer.byteLength(raw).toString());
-      expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
-      expect(response.statusCode).toBe(200);
-    }
-  });
-
-  it('should correctly overwrite headers after null serialization, if specified in the interceptor', async () => {
+it('should correctly serialize null returned by interceptor', async () => {
+  // sync
+  {
     const httpRouter: HttpRouter = new HttpRouter();
 
     httpRouter
       .intercept((request, response, next) => {
-        return next.handle().pipe(map(() => {
-          response
-            .setHeader('content-length', '4')
-            .setHeader('content-type', 'application/json');
-
-          return EXAMPLE_NULL;
-        }));
+        return next.handle().pipe(map(() => EXAMPLE_NULL));
       })
       .route({ handler: () => undefined, method: 'GET', path: '/' });
 
@@ -127,10 +20,115 @@ describe('interceptor serialize null', () => {
       httpRouter.handle(request, response);
     });
 
+    const raw: string = JSON.stringify(EXAMPLE_NULL);
+
     const response = await supertest(httpServer)
       .get('/');
 
-    expect(response.headers['content-length']).toBe('4');
-    expect(response.headers['content-type']).toBe('application/json');
+    expect(response.body).toEqual(EXAMPLE_NULL);
+    expect(response.headers['content-length']).toBe(Buffer.byteLength(raw).toString());
+    expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+    expect(response.statusCode).toBe(200);
+  }
+
+  // async
+  {
+    const httpRouter: HttpRouter = new HttpRouter();
+
+    httpRouter
+      .intercept((request, response, next) => {
+        return next.handle().pipe(exhaustMap(() => Promise.resolve(EXAMPLE_NULL)));
+      })
+      .route({ handler: () => undefined, method: 'GET', path: '/' });
+
+    const httpServer: http.Server = http.createServer((request, response) => {
+      httpRouter.handle(request, response);
+    });
+
+    const raw: string = JSON.stringify(EXAMPLE_NULL);
+
+    const response = await supertest(httpServer)
+      .get('/');
+
+    expect(response.body).toEqual(EXAMPLE_NULL);
+    expect(response.headers['content-length']).toBe(Buffer.byteLength(raw).toString());
+    expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+    expect(response.statusCode).toBe(200);
+  }
+
+  // sync + observable
+  {
+    const httpRouter: HttpRouter = new HttpRouter();
+
+    httpRouter
+      .intercept((request, response, next) => {
+        return next.handle().pipe(exhaustMap(() => of(EXAMPLE_NULL)));
+      })
+      .route({ handler: () => undefined, method: 'GET', path: '/' });
+
+    const httpServer: http.Server = http.createServer((request, response) => {
+      httpRouter.handle(request, response);
+    });
+
+    const raw: string = JSON.stringify(EXAMPLE_NULL);
+
+    const response = await supertest(httpServer)
+      .get('/');
+
+    expect(response.body).toEqual(EXAMPLE_NULL);
+    expect(response.headers['content-length']).toBe(Buffer.byteLength(raw).toString());
+    expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+    expect(response.statusCode).toBe(200);
+  }
+
+  // async + observable
+  {
+    const httpRouter: HttpRouter = new HttpRouter();
+
+    httpRouter
+      .intercept((request, response, next) => {
+        return next.handle().pipe(exhaustMap(() => Promise.resolve(of(EXAMPLE_NULL))), mergeAll());
+      })
+      .route({ handler: () => undefined, method: 'GET', path: '/' });
+
+    const httpServer: http.Server = http.createServer((request, response) => {
+      httpRouter.handle(request, response);
+    });
+
+    const raw: string = JSON.stringify(EXAMPLE_NULL);
+
+    const response = await supertest(httpServer)
+      .get('/');
+
+    expect(response.body).toEqual(EXAMPLE_NULL);
+    expect(response.headers['content-length']).toBe(Buffer.byteLength(raw).toString());
+    expect(response.headers['content-type']).toBe('application/json; charset=utf-8');
+    expect(response.statusCode).toBe(200);
+  }
+});
+
+it('should correctly overwrite headers after null serialization, if specified in the interceptor', async () => {
+  const httpRouter: HttpRouter = new HttpRouter();
+
+  httpRouter
+    .intercept((request, response, next) => {
+      return next.handle().pipe(map(() => {
+        response
+          .setHeader('content-length', '4')
+          .setHeader('content-type', 'application/json');
+
+        return EXAMPLE_NULL;
+      }));
+    })
+    .route({ handler: () => undefined, method: 'GET', path: '/' });
+
+  const httpServer: http.Server = http.createServer((request, response) => {
+    httpRouter.handle(request, response);
   });
+
+  const response = await supertest(httpServer)
+    .get('/');
+
+  expect(response.headers['content-length']).toBe('4');
+  expect(response.headers['content-type']).toBe('application/json');
 });
