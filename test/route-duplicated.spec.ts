99cf8e2a856c34a31abeac690c2a64d5f74e6933
Paweł Partyka <partyka95@icloud.com>
2022-07-06 20:48:14 +0200

    test: route duplicated

--- test/route-duplicated.spec.ts
+++ test/route-duplicated.spec.ts
@@ -11,3 +11,15 @@ it('should thrown an Error if duplicate routes are detected', () => {
     expect(error.message).toBe('Duplicated {GET /pigs} http route');
   }
 });
+
+it('should thrown an Error if duplicate routes with optional parameters are detected', () => {
+  try {
+    const httpRouter: HttpRouter = new HttpRouter();
+
+    httpRouter
+      .route({ handler: () => '', method: 'GET', path: '/pigs/:id' })
+      .route({ handler: () => '', method: 'GET', path: '/pigs/:name?' });
+  } catch (error) {
+    expect(error.message).toBe('Duplicated {GET /pigs/:name?} http route');
+  }
+});
