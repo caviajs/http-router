0bb3091d70776d9fd7f23eb7ce1dd209bff4d9d4
Paweł Partyka <partyka95@icloud.com>
2022-10-02 01:39:33 +0200

    fix: release script

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ VERSION=$(release-it --release-version --npm.skipChecks)
 
 npm version "$VERSION" --no-git-tag-version
 npm run build
-npm publish
+npm publish --access public
 
 git add package.json package-lock.json
 git commit -m "chore: release v$VERSION"
