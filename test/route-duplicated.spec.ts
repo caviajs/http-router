b453a4453fbd452ca2f2812dd1de30f9efad34b7
Paweł Partyka <partyka95@icloud.com>
2023-11-25 14:25:46 +0100

    test: route duplicated

--- test/route-duplicated.spec.ts
+++ test/route-duplicated.spec.ts
@@ -1,25 +1,85 @@
 import { HttpRouter } from '../src';
 
-it('should thrown an Error if duplicate routes are detected', () => {
+it('should thrown an Error if duplicate routes are detected (static + static)', done => {
   try {
-    const httpRouter: HttpRouter = new HttpRouter();
-
-    httpRouter
+    new HttpRouter()
       .route({ handler: () => '', method: 'GET', path: '/pigs' })
       .route({ handler: () => '', method: 'GET', path: '/pigs' });
   } catch (error) {
     expect(error.message).toBe('Duplicated {GET /pigs} http route');
+
+    done();
   }
 });
 
-it('should thrown an Error if duplicate routes are detected (optional parameters)', () => {
+it('should thrown an Error if duplicate routes are detected (static + parameter)', done => {
   try {
-    const httpRouter: HttpRouter = new HttpRouter();
+    new HttpRouter()
+      .route({ handler: () => '', method: 'GET', path: '/pigs/list' })
+      .route({ handler: () => '', method: 'GET', path: '/pigs/:id' });
+  } catch (error) {
+    expect(error.message).toBe('Duplicated {GET /pigs/:id} http route');
 
-    httpRouter
+    done();
+  }
+});
+
+it('should thrown an Error if duplicate routes are detected (static + optional parameter)', done => {
+  try {
+    new HttpRouter()
+      .route({ handler: () => '', method: 'GET', path: '/pigs/list' })
+      .route({ handler: () => '', method: 'GET', path: '/pigs/:id?' });
+  } catch (error) {
+    expect(error.message).toBe('Duplicated {GET /pigs/:id?} http route');
+
+    done();
+  }
+});
+
+it('should thrown an Error if duplicate routes are detected (parameter + static)', done => {
+  try {
+    new HttpRouter()
+      .route({ handler: () => '', method: 'GET', path: '/pigs/:id' })
+      .route({ handler: () => '', method: 'GET', path: '/pigs/list' });
+  } catch (error) {
+    expect(error.message).toBe('Duplicated {GET /pigs/list} http route');
+
+    done();
+  }
+});
+
+it('should thrown an Error if duplicate routes are detected (optional parameter + static)', done => {
+  try {
+    new HttpRouter()
+      .route({ handler: () => '', method: 'GET', path: '/pigs/:id?' })
+      .route({ handler: () => '', method: 'GET', path: '/pigs/list' });
+  } catch (error) {
+    expect(error.message).toBe('Duplicated {GET /pigs/list} http route');
+
+    done();
+  }
+});
+
+it('should thrown an Error if duplicate routes are detected (parameter + optional parameter)', done => {
+  try {
+    new HttpRouter()
       .route({ handler: () => '', method: 'GET', path: '/pigs/:id' })
       .route({ handler: () => '', method: 'GET', path: '/pigs/:name?' });
   } catch (error) {
     expect(error.message).toBe('Duplicated {GET /pigs/:name?} http route');
+
+    done();
+  }
+});
+
+it('should thrown an Error if duplicate routes are detected (optional parameter + parameter)', done => {
+  try {
+    new HttpRouter()
+      .route({ handler: () => '', method: 'GET', path: '/pigs/:name?' })
+      .route({ handler: () => '', method: 'GET', path: '/pigs/:id' });
+  } catch (error) {
+    expect(error.message).toBe('Duplicated {GET /pigs/:id} http route');
+
+    done();
   }
 });
